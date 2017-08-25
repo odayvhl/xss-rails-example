540dd3199a3a69ff6bcd4f4709df9ef3e56ce194
Oliver Day <oday@vistahigherlearning.com>
2017-08-25 15:37:38 -0400

    Change default route to articles index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -8,7 +6,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'articles#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
